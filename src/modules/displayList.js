0927ae6685fc529d41a8184509628bfe881a0e06
Raj Shekhar <rajshekhar8926@gmail.com>
2021-06-05 15:51:43 +0530

    Add animations when navbar opens

--- src/modules/displayList.js
+++ src/modules/displayList.js
@@ -2,13 +2,14 @@ const btnHamburger = document.querySelector('.fa-bars');
 
 const displayList = () => {
 	const navUl = document.querySelector('nav ul');
-	navUl.classList.toggle('display-nav');
 	if (btnHamburger.classList.contains('fa-bars')) {
 		btnHamburger.classList.remove('fa-bars');
 		btnHamburger.classList.add('fa-times');
+		navUl.style.width = '100%';
 	} else {
 		btnHamburger.classList.remove('fa-times');
 		btnHamburger.classList.add('fa-bars');
+		navUl.style.width = '0';
 	}
 };
 

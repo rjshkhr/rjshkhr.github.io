0546e238776fb84e5bd203332a971be7bf149500
rajshekhar26 <rjshekhar.official@gmail.com>
2021-08-22 18:02:25 +0530

    Setup eslint and prettier

--- src/modules/toggleTheme.js
+++ src/modules/toggleTheme.js
@@ -11,13 +11,12 @@ const setTheme = (bodyClass, buttonClass) => {
 	btnTheme.classList.add(buttonClass);
 	localStorage.setItem('class-body-theme', bodyClass);
 	localStorage.setItem('class-btn-theme', buttonClass);
-}
+};
 
-const toggleTheme = () => (
-  isDark()
-  ? setTheme('body-light', 'fa-moon')
-  : setTheme('body-dark', 'fa-sun')
-);
+const toggleTheme = () =>
+	isDark()
+		? setTheme('body-light', 'fa-moon')
+		: setTheme('body-dark', 'fa-sun');
 
 const addBodyClass = () => document.body.classList.add(getBodyClass);
 const addBtnClass = () => btnTheme.classList.add(getBtnClass);

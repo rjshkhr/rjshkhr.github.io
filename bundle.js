f8e2c0bacea2de043a5ecbca7125bcf48d5c656c
rajshekhar26 <rjshekhar.official@gmail.com>
2021-09-11 18:27:55 +0530

    Updates

--- bundle.js
+++ bundle.js
@@ -1,7 +1,48 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 var __webpack_exports__ = {};
 
+;// CONCATENATED MODULE: ./src/assets/images/cleanfolio.png
+/* harmony default export */ const cleanfolio = (__webpack_require__.p + "assets/images/cleanfolio.png");
+;// CONCATENATED MODULE: ./src/assets/images/startpage.png
+/* harmony default export */ const startpage = (__webpack_require__.p + "assets/images/startpage.png");
 ;// CONCATENATED MODULE: ./src/modules/scroll.js
 var scrollUp = function scrollUp() {
   var btnScrollUp = document.querySelector('.scroll-up');
@@ -56,16 +97,18 @@ var displayList = function displayList() {
   if (btnHamburger.classList.contains('fa-bars')) {
     btnHamburger.classList.remove('fa-bars');
     btnHamburger.classList.add('fa-times');
-    navUl.style.width = '100%';
+    navUl.classList.add('display-nav-list');
   } else {
     btnHamburger.classList.remove('fa-times');
     btnHamburger.classList.add('fa-bars');
-    navUl.style.width = '0';
+    navUl.classList.remove('display-nav-list');
   }
 };
 
 /* harmony default export */ const modules_displayList = (displayList);
 ;// CONCATENATED MODULE: ./src/index.js
+
+
 
 
 

291d7448af4c5e4046b2dc2cfc0d2de2dc631b89
rajshekhar26 <rjshekhar.official@gmail.com>
2021-10-06 13:38:02 +0530

    Add html-loader and eslint:fix script

--- webpack.config.js
+++ webpack.config.js
@@ -5,54 +5,52 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-	output: {
-		path: path.join(__dirname, '/dist'),
-		filename: 'bundle.js',
-	},
-	devServer: {
-		static: path.join(__dirname, '/dist'),
-		hot: true,
-	},
-	devtool: 'source-map',
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-					},
-				},
-			},
-			{
-				test: /\.css$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader'],
-			},
-			{
-				test: /\.(png|jpe?g|gif)$/i,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: '[name].[ext]',
-							outputPath: 'assets/images/',
-						},
-					},
-				],
-			},
-		],
-	},
-	optimization: {
-		minimizer: [new CssMinimizerPlugin()],
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-			favicon: './src/favicon.png',
-		}),
-		new MiniCssExtractPlugin(),
-	],
-};
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true,
+  },
+  devServer: {
+    hot: true,
+    open: true,
+    port: 3000,
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: './src/favicon.png',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+}
